7ce10416a78956ffda4595a0fff56fcd389ccdb2
Riccardo Ficarra <riccardo.ficarra95@gmail.com>
2018-12-05 12:49:55 -0600

    Added function to print all characters in all books

--- src/relationExtraction/Main.java
+++ src/relationExtraction/Main.java
@@ -7,5 +7,15 @@ public class Main {
         String path = args[0];
         CharacterRelationParser crp = new CharacterRelationParser(path);
         HashMap<String, Book> books = crp.parseCharacterRelations();
+        printCharacters(books);
+
     }
+
+    static void printCharacters(HashMap<String, Book> books) {
+        books.values().forEach(b -> {
+            System.out.println(b.getTitle());
+            b.getCharacters().forEach(c -> System.out.println("\t" + c));
+        });
+    }
+
 }

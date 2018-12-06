cb33907d241ac973ae3c8dae35fe272297a63684
Riccardo Ficarra <riccardo.ficarra95@gmail.com>
2018-12-06 15:05:08 -0600

    Corrected accumulation into labeledSentence hashmap

--- src/relationExtraction/NaiveBayes.java
+++ src/relationExtraction/NaiveBayes.java
@@ -3,6 +3,7 @@ package relationExtraction;
 import net.sf.javaml.classification.bayes.NaiveBayesClassifier;
 import net.sf.javaml.core.DefaultDataset;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -20,10 +21,21 @@ class NaiveBayes {
 
     void buildModel(HashMap<String, Book> books) {
         DefaultDataset dataset = new DefaultDataset();
+
+        //Dataset Building
         HashMap<String, List<Sentence>> labeledSentences = new HashMap<>();
         for (Book book : books.values()) {
-            labeledSentences.putAll((book.buildRelationSentence(relationlabel)));
+            HashMap<String, List<Sentence>> bookLabeledSentences = book.buildRelationSentence(relationlabel);
+            for (String relation : bookLabeledSentences.keySet()) {
+                labeledSentences.merge(relation, bookLabeledSentences.get(relation), (l1, l2) -> {
+                    List<Sentence> newList = new ArrayList<>();
+                    newList.addAll(l1);
+                    newList.addAll(l2);
+                    return newList;
+                });
+            }
         }
+
     }
 
 }
